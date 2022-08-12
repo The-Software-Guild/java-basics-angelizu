ed8aafea8cb48a21812e97706a4f2390d1fc7b7a
angel.izu <angelizux@yahoo.com>
2022-08-12 10:11:27 +0100

    added comments

--- src/Exercise3/HealthyHearts.java
+++ src/Exercise3/HealthyHearts.java
@@ -10,10 +10,12 @@ public class HealthyHearts {
         System.out.print("What's your age? ");
         age = Integer.parseInt(scanner.nextLine());
 
+        // rounds up the float number
         maxRate = 220 - age;
         System.out.println("Your maximum heart rate should be " + Math.round(maxRate)
                 + " beats per minute.");
 
+        // rounds up each of the two ranges
         zone1 = maxRate * 50 / 100;
         zone2 = maxRate * 85 / 100;
         System.out.println("Your target HR Zone is " + Math.round(zone1) +
